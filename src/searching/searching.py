37a393f77e0366638032c0d6525d08e2a00a3520
Michelle Scott <scottmm374@gmail.com>
2020-09-02 09:20:47 -0400

    linear pass

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,7 +1,13 @@
+
+arr1 = [-2, 7, 3, -9, 5, 1, 0, 4, -6]
+arr2 = []
+
+
 def linear_search(arr, target):
-    # Your code here
-
-
+    # Check index = to target
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
     return -1   # not found
 
 
@@ -10,5 +16,8 @@ def binary_search(arr, target):
 
     # Your code here
 
-
     return -1  # not found
+
+
+lin_search = linear_search(arr1, -9)
+print(lin_search)
